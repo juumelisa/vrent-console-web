22778e890666beee73afb62101c2a70e3f408f7c
juumelisa <jumelisah@gmail.com>
2025-11-23 15:35:08 +0700

    pass token to api

--- src/server/apiClient.ts
+++ src/server/apiClient.ts
@@ -1,10 +1,17 @@
+import { cookies } from "next/headers";
+
 const apiUrl = process.env.API_URL
 const apiKey = process.env.API_KEY || ''
 
 export async function apiClient (path: string, options: RequestInit = {}) {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("token")
+  const tokenValue = token?.value ?? ""
+
   const headers: Record<string, string> = {
     "Content-Type": "application/json",
     "x-api-key": apiKey,
+    "token": tokenValue,
     ...(options.headers as Record<string, string>)
   }
 
